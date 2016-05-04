3518c92e90b1c7ebc7900228f946fdb596b0cc87
Eric Golde <eric@golde.org>
2016-05-04 09:04:13 -0700

    Auto update (FROM ATHLON)

---
+++ Push.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Username: egold555
+echo Password: ericgolde555
+echo Adding...
+git add .
+echo Commiting...
+git commit -m "Auto update (FROM ATHLON)"
+echo Pushing...
+git push
+echo Done!
+pause
---
+++ Update.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Cloning repo......
+git pull https://github.com/egold555/Social-Innovations.git
+echo done!
+pause
