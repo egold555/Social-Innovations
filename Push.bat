14435168be2cf15c808377fec376c298a9e3fdbe
Eric Golde <eric@golde.org>
2016-05-09 12:37:42 -0700

    Added buttons to go to our essays. Also added essays.html that shows our essays.

--- Push.bat
+++ Push.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Adding...
 git add .
-git commit -m "Added hyperlink to twitter"
+git commit -m "Added buttons to go to our essays. Also added essays.html that shows our essays."
 echo Pushing...
 git push
 echo Done!
