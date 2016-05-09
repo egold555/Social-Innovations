599cf2616396a9e9d108d42d4b8292cb337058ab
Eric Golde <eric@golde.org>
2016-05-09 12:45:54 -0700

    Added blank page template and worked on twitter feed.

--- Push.bat
+++ Push.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Adding...
 git add .
-git commit -m "Added buttons to go to our essays. Also added essays.html that shows our essays."
+git commit -m "Added blank page template and worked on twitter feed."
 echo Pushing...
 git push
 echo Done!
