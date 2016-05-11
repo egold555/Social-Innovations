8e379745984952ed786fce25e00873b10dc9132e
Eric Golde <eric@golde.org>
2016-05-10 20:18:50 -0700

    Fixed all UTF-8 formatting conflictions with characters turning into the '∩┐╜' character. Fixed all broken links and links opening in new tabs. Removed old unnessary files. (Except images)

--- Push.bat
+++ Push.bat
@@ -1,7 +1,7 @@
-@echo off
+﻿@echo off
 echo Adding...
 git add .
-git commit -m "Wouldent let me pull without pushing -thanks github "
+git commit -m "Fixed all UTF-8 formatting conflictions with characters turning into the '�' character. Fixed all broken links and links opening in new tabs. Removed old unnessary files. (Except images)"
 echo Pushing...
 git push
 echo Done!
