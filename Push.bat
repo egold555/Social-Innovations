839ed88e488d2d73e369fe1eed8120d623bf74a4
Eric Golde <eric@golde.org>
2016-05-10 20:04:10 -0700

    Wouldent let me pull without pushing -thanks github

--- Push.bat
+++ Push.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Adding...
 git add .
-git commit -m "Magical stock images are gone! :D"
+git commit -m "Wouldent let me pull without pushing -thanks github "
 echo Pushing...
 git push
 echo Done!
